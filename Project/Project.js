95b2ea2698ecb8c536323f7c5fb57d6b32642778
eroinnovations <eroinnovations9@gmail.com>
2025-06-03 12:42:58 +0300

    Da

--- Project/Project.js
+++ Project/Project.js
@@ -1,4 +1,14 @@
+const URL='https://docs.google.com/spreadsheets/d/1x_Nruw60vwcWJKkMUWpNncGzQSoPgHoMP2X8so3eS8I/edit?usp=sharing';
+
 const NOVASTART=()=>{
+    
+    VISITOR(URL,'Users');
+
+    ROUTE('',HOMEPAGE,'HOMEPAGE');
+
+};
+
+const HOMEPAGEROUTER=()=>{
 
     ROUTE('',HOMEPAGE,'HOMEPAGE');
 
@@ -8,8 +18,32 @@ const HOMEPAGE=()=>{
 
     DISPLAY('',`
 
-        <h1>Start</h1>
+        <header>
+
+            <div id='HeaderHolder' class='TopNav'>
+
+                <img id='Logo' class='LeftIcon' src='https://ashshakurcharity.org/Assets/logo.jpg'/>
         
+                <h1 class='CompanyName'>Ash Shakur Charity and Orphanage Aid</h1>
+
+                <p class='Motto'>"Helping the poor orphans and the needy people"</p>
+
+                <img class='RightIcon' src='${WHITEMENUICON}'/>
+
+            </div>
+
+            <div id='DonateSection' class='TopNav'>
+
+                <h1>Donate</h1>
+            
+            </div>
+
+        </header>
+
+        <div class=''>
+        
+        </div>
+
     `);
 
 };
