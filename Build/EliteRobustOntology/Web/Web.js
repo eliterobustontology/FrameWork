7cb78cc528f53577e57954e81d1cef93dbe6c0a1
eroinnovations <eroinnovations9@gmail.com>
2025-06-06 02:16:00 +0300

    Updates

--- Build/EliteRobustOntology/Web/Web.js
+++ Build/EliteRobustOntology/Web/Web.js
@@ -19,6 +19,24 @@ const HOMEPAGE=()=>{
             <img class='RightIcon' src='${WHITEMENUICON}'/>
         
         </header>
+
+        <div class='HomeDiv'>
+
+            <br>
+
+            <h1>Welcome </h1>
+
+            <p class='LeftText'>Elite Robust Ontology is a Cloud Native Based Software Development Company Based in Mbale Uganda.</p>
+        
+            <br>
+
+            <p class='LeftText'>We Venture in Mobile App Development,Desktop App Development ,Website Development ,System Design and Development ,Web Hosting etc...</p>
+
+            <br>
+
+            <h1>Our Mission</h1>
+
+        </div>
         
     `);
 
---
+++ Project/Project.js
@@ -0,0 +1,43 @@
+const NOVASTART=()=>{
+
+    APPMODE('#333333');
+
+    ROUTE('',HOMEPAGE,HOMEPAGE);
+
+};
+
+const HOMEPAGE=()=>{
+
+    DISPLAY('',`
+        
+        <header>
+
+            <img class='LeftIcon' src='https://eroinnovations.github.io/FrameWork/Library/Assets/Images/Posts/Elite/CompanyLogo.png'/>
+
+            <p>Elite Robust Ontology</p>
+
+            <img class='RightIcon' src='${WHITEMENUICON}'/>
+        
+        </header>
+
+        <div class='HomeDiv'>
+
+            <br>
+
+            <h1>Welcome </h1>
+
+            <p class='LeftText'>Elite Robust Ontology is a Cloud Native Based Software Development Company Based in Mbale Uganda.</p>
+        
+            <br>
+
+            <p class='LeftText'>We Venture in Mobile App Development,Desktop App Development ,Website Development ,System Design and Development ,Web Hosting etc...</p>
+
+            <br>
+
+            <h1>Our Mission</h1>
+
+        </div>
+        
+    `);
+
+};
