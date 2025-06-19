955996478c4df9a860f1548abc7df244fea36cff
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-06-19 21:55:21 +0300

    Updates

---
+++ Build/SalimuSaley/Web/Web.js
@@ -0,0 +1,19 @@
+const NOVASTART=()=>{
+
+    ROUTE("",HOMEPAGE,HOMEPAGE);
+
+};
+
+const HOMEPAGE=()=>{
+
+    DISPLAY("",`
+
+        <header>
+
+            <h1>Salimu Saley Muslim Charity Organisation</h1>
+        
+        </header>
+        
+    `);
+
+};
