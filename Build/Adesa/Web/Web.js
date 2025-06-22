3283296f0eef2612167018465146b9228a11394b
EroInnovations <eroinnovations9@gmail.com>
2025-06-23 00:42:15 +0300

    Updates

---
+++ Build/Adesa/Web/Web.js
@@ -0,0 +1,21 @@
+const NOVASTART=()=>{
+
+    ROUTE("",HOMEPAGE,HOMEPAGE);
+
+};
+
+const HOMEPAGEROUTER=()=>{
+
+    ROUTE("",HOMEPAGE,HOMEPAGE);
+
+}
+
+const HOMEPAGE=()=>{
+
+    DISPLAY('',`
+
+        <h1>Launching Soon</h1>
+        
+    `);
+
+};
--- Project/Project.js
+++ Project/Project.js
@@ -8,127 +8,13 @@ const HOMEPAGEROUTER=()=>{
 
     ROUTE("",HOMEPAGE,HOMEPAGE);
 
-};
+}
 
 const HOMEPAGE=()=>{
 
-    DISPLAY("",`
+    DISPLAY('',`
 
-        <header class="AppHeader" >
-
-            <h1 class="TopCompanyName">SSMCO</h1>
-
-            <p class="CompanyName">Salimu Saley Muslim Charity Organisation</p>
-        
-            <header class="SubAppHeader">
-
-                <h1 class="Menusections" onclick="ABOUTUSPAGEROUTER()">About</h1>
-
-                <h1 class="Menusections" onclick="SERVICESPAGEPAGEROUTER()">Services</h1>
-
-                <h1 class="Menusections" onclick="CONTACTUSPAGEROUTER()">Contact</h1>
-            
-            </header>
-
-        </header>
-
-        <div  class="HeaderDiv"></div>
-        
-    `);
-
-};
-
-const ABOUTUSPAGEROUTER=()=>{
-
-    ROUTE(" ",ABOUTUSPAGE,HOMEPAGE);
-
-};
-
-const ABOUTUSPAGE=()=>{
-
-    DISPLAY("",`
-
-        <header class="AppHeader" >
-
-            <h1 class="TopCompanyName">SSMCO</h1>
-
-            <p class="CompanyName">Salimu Saley Muslim Charity Organisation</p>
-        
-            <header class="SubAppHeader">
-
-                <img onclick="HOMEPAGEROUTER()" class="LeftIcon" src="${WHITEBACKICON}"/>
-                
-                <h1 id="SectionText" class="RightText" >About Us</h1>
-            
-            </header>
-
-        </header>
-
-        <div  class="HeaderDiv"></div>
-        
-    `);
-
-};
-
-const SERVICESPAGEPAGEROUTER=()=>{
-
-    ROUTE(" ",SERVICESPAGE,HOMEPAGE);
-
-};
-
-const SERVICESPAGE=()=>{
-
-    DISPLAY("",`
-
-        <header class="AppHeader" >
-
-            <h1 class="TopCompanyName">SSMCO</h1>
-
-            <p class="CompanyName">Salimu Saley Muslim Charity Organisation</p>
-        
-            <header class="SubAppHeader">
-
-                <img onclick="HOMEPAGEROUTER()" class="LeftIcon" src="${WHITEBACKICON}"/>
-                
-                <h1 id="SectionText" class="RightText" >Our Services</h1>
-            
-            </header>
-
-        </header>
-
-        <div  class="HeaderDiv"></div>
-        
-    `);
-
-};
-
-const CONTACTUSPAGEROUTER=()=>{
-
-    ROUTE(" ",CONTACTUSPAGE,HOMEPAGE);
-
-};
-
-const CONTACTUSPAGE=()=>{
-
-    DISPLAY("",`
-
-        <header class="AppHeader" >
-
-            <h1 class="TopCompanyName">SSMCO</h1>
-
-            <p class="CompanyName">Salimu Saley Muslim Charity Organisation</p>
-        
-            <header class="SubAppHeader">
-
-                <img onclick="HOMEPAGEROUTER()" class="LeftIcon" src="${WHITEBACKICON}"/>
-                
-                <h1 id="SectionText" class="RightText" >Contact Us</h1>
-            
-            </header>
-
-        </header>
-
-        <div  class="HeaderDiv"></div>
+        <h1>Launching Soon</h1>
         
     `);
 
