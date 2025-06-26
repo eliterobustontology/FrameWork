f29f8e2c2c7cc8e34a5c7b278a05c760cf717e46
EroInnovations <eroinnovations9@gmail.com>
2025-06-26 22:02:35 +0300

    Updates

--- Build/AshStoryPosts/Desktop/Desktop.js
+++ Build/AshStoryPosts/Desktop/Desktop.js
@@ -2,7 +2,23 @@ const NOVASTART=()=>{
 
     DISPLAY('',`
 
-        <h1>Under Development</h1>
+        <header>
+
+            <h1>ASCO MANAGER</h1>
+        
+        </header>
+
+        <div class='MenuSection'>
+
+            <button class='MenuButtons' >Create Project</button>
+
+            <button class='MenuButtons' >All Projects</button>
+
+            <button class='MenuButtons' onclick='RELOAD()'>App Sync</button>
+        
+        </div>
+
+        <div class='DataSection' ></div>
         
     `);
 
